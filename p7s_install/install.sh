0f414f7611cf1b577484f031e80a304c03e80350
DamageControlStudio <24506176+DamageControlStudio@users.noreply.github.com>
2019-11-16 13:10:28 +0800

    Add files via upload

--- p7s_install/install.sh
+++ p7s_install/install.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-# wget https://github.com/DamageControlStudio/Downloads/raw/master/p7s.zip
+# 解压安装包
 unzip -q p7s.zip
 
-rm p7s.zip
-rm p7s_local.py
-rm p7s_sqlite.db
-rm README.md
-rm -rf __MAXOSX
-rm explore.ipynb
-
+# 清理残存数据
 rm /usr/share/nginx/html/p7s_sqlite.db
 rm -rf /usr/share/nginx/html/files
 rm -rf /usr/share/nginx/html/p7s_env
 
+# 安装必要环境
 apt-get update
 apt-get install nginx python-virtualenv ffmpeg
 
+# 创建工作文件夹
 mkdir /usr/share/nginx/html/templates
 mkdir /usr/share/nginx/html/files
 mkdir /usr/share/nginx/html/logs
 
-mv p7s_setup.py /usr/share/nginx/html/p7s_setup.py
+# 所谓安装：众神归位
+mv p7s_init.py /usr/share/nginx/html/p7s_init.py
 mv p7s_main.py /usr/share/nginx/html/p7s_main.py
 mv p7s_download.py /usr/share/nginx/html/p7s_download.py
 mv p7s_utils.py /usr/share/nginx/html/p7s_utils.py
@@ -29,12 +26,20 @@ mv p7s_startup.sh /usr/share/nginx/html/p7s_startup.sh
 mv templates/status.html /usr/share/nginx/html/templates/status.html
 mv templates/welcome.html /usr/share/nginx/html/templates/welcome.html
 
+# 删除没用的文件
+rm p7s.zip
+rm -rf __MAXOSX
+rm -rf templates
+
+# 创建虚拟环境 p7s_env 
 cd /usr/share/nginx/html
 virtualenv --no-site-packages p7s_env --python=python3
 source p7s_env/bin/activate
 
+# 安装必要环境
 pip install -U gunicorn flask youtube-dl
 
+# 配置 nginx
 cat > /etc/nginx/sites-available/default << EOF
 server {
     listen 80;
@@ -47,7 +52,6 @@ server {
 }
 EOF
 
+# 初始化 p7s
+python p7s_init.py
 chmod -R 777 /usr/share/nginx/html
-
-python p7s_setup.py
-
